7c8d1c673d01c191c3d14b64637ace00c57e78d3
Nik Graf <nik@nikgraf.com>
2016-04-07 21:11:10 -0700

    remove accordion

--- examples/Stylesheets.elm
+++ examples/Stylesheets.elm
@@ -1,7 +1,6 @@
 module Stylesheets (..) where
 
 import Css.File exposing (..)
-import Belle.Accordion as Accordion
 import Belle.Button as Button
 import Belle.Rating as Rating
 
@@ -9,7 +8,6 @@ import Belle.Rating as Rating
 port files : CssFileStructure
 port files =
   toFileStructure
-    [ ( "accordion-example.css", compile Accordion.css )
-    , ( "button-example.css", compile Button.css )
+    [ ( "button-example.css", compile Button.css )
     , ( "rating-example.css", compile Rating.css )
     ]
