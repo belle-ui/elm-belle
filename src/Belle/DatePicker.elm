e8061745c32d4d7c05935c84587dbf2ee5126691
Tereza Sokol <terezasokol@gmail.com>
2016-05-01 22:54:19 +0200

    fix typo in variable

--- src/Belle/DatePicker.elm
+++ src/Belle/DatePicker.elm
@@ -128,13 +128,13 @@ createDays address (day, month, year) choosen =
 
 
 viewDay : Signal.Address Action -> DateTuple -> DateTuple -> Html
-viewDay address (day, month, year) (choosenDay, changeMonth, choosenYear) =
+viewDay address (day, month, year) (choosenDay, choosenMonth, choosenYear) =
   let
     date =
       (day, month, year)
 
     highlight =
-      day == choosenDay && month == changeMonth && year == choosenYear
+      day == choosenDay && month == choosenMonth && year == choosenYear
 
     classes =
       [ ( "BelleDatePickerDay", True )
